35833db5ece4ac7c8a693c1303eca17f3698057c
pliebscher <ppliebscher@gmail.com>
2015-09-23 09:16:55 -0700

    Added new Settings dialog.

---
+++ Snoopy.TimeSeries.SampleSniffer/Snoopy.TimeSeries.UI/UI/Forms/FormSettings.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormSettings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormSettings))
+        Me.cbInputDevice = New System.Windows.Forms.ComboBox()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.cbSampleSize = New System.Windows.Forms.ComboBox()
+        Me.cbSampleRate = New System.Windows.Forms.ComboBox()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SuspendLayout()
+        '
+        'cbInputDevice
+        '
+        Me.cbInputDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbInputDevice.FormattingEnabled = True
+        Me.cbInputDevice.Location = New System.Drawing.Point(108, 24)
+        Me.cbInputDevice.Name = "cbInputDevice"
+        Me.cbInputDevice.Size = New System.Drawing.Size(128, 21)
+        Me.cbInputDevice.TabIndex = 2
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Location = New System.Drawing.Point(12, 27)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(71, 13)
+        Me.label5.TabIndex = 35
+        Me.label5.Text = "Input Device:"
+        '
+        'cbSampleSize
+        '
+        Me.cbSampleSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbSampleSize.FormattingEnabled = True
+        Me.cbSampleSize.Items.AddRange(New Object() {"1024", "2048", "4096", "8192", "16384", "32768"})
+        Me.cbSampleSize.Location = New System.Drawing.Point(108, 78)
+        Me.cbSampleSize.Name = "cbSampleSize"
+        Me.cbSampleSize.Size = New System.Drawing.Size(128, 21)
+        Me.cbSampleSize.TabIndex = 4
+        '
+        'cbSampleRate
+        '
+        Me.cbSampleRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbSampleRate.FormattingEnabled = True
+        Me.cbSampleRate.Items.AddRange(New Object() {"8000", "11025", "22050", "44100", "48000", "96000"})
+        Me.cbSampleRate.Location = New System.Drawing.Point(108, 51)
+        Me.cbSampleRate.Name = "cbSampleRate"
+        Me.cbSampleRate.Size = New System.Drawing.Size(128, 21)
+        Me.cbSampleRate.TabIndex = 3
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(12, 81)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(76, 13)
+        Me.label2.TabIndex = 30
+        Me.label2.Text = "Bytes / Frame:"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(12, 54)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(71, 13)
+        Me.label1.TabIndex = 29
+        Me.label1.Text = "Sample Rate:"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(85, 113)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnOK
+        '
+        Me.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.btnOK.Location = New System.Drawing.Point(167, 113)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 5
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 7)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(234, 100)
+        Me.GroupBox1.TabIndex = 39
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Capture"
+        '
+        'FormSettings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 142)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.cbInputDevice)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.cbSampleSize)
+        Me.Controls.Add(Me.cbSampleRate)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormSettings"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Settings"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents cbInputDevice As System.Windows.Forms.ComboBox
+    Private WithEvents label5 As System.Windows.Forms.Label
+    Private WithEvents cbSampleSize As System.Windows.Forms.ComboBox
+    Private WithEvents cbSampleRate As System.Windows.Forms.ComboBox
+    Private WithEvents label2 As System.Windows.Forms.Label
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Private WithEvents btnCancel As System.Windows.Forms.Button
+    Private WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+End Class
---
+++ Snoopy.TimeSeries.SampleSniffer/Snoopy.TimeSeries.UI/UI/Forms/FormSettings.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Runtime.InteropServices
+Public Class FormSettings
+
+    Private Sub FormSettings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        cbSampleRate.SelectedItem = App.SampleRate.ToString
+        cbSampleSize.SelectedItem = App.SampleSize.ToString
+
+        If WaveNative.waveInGetNumDevs() = 0 Then
+            cbInputDevice.Items.Add("[No Devices]")
+            cbInputDevice.SelectedIndex = 0
+            cbInputDevice.Enabled = False
+            cbSampleRate.Enabled = False
+            cbSampleSize.Enabled = False
+            Exit Sub
+        End If
+
+        cbInputDevice.Items.Add("[Default]")
+
+        For i As Integer = 0 To WaveNative.waveInGetNumDevs() - 1
+
+            Dim dev As New WaveNative.WaveInCaps
+            WaveNative.waveInGetDevCaps(i, dev, CUInt(Marshal.SizeOf(dev)))
+
+            cbInputDevice.Items.Add(dev.szPname)
+
+        Next
+
+        cbInputDevice.SelectedIndex = App.AudioInputDevice + 1 ' (Add 1 to offset the default of -1)
+
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+
+        App.SampleRate = CInt(cbSampleRate.SelectedItem)
+        App.SampleSize = CInt(cbSampleSize.SelectedItem)
+        App.AudioInputDevice = cbInputDevice.SelectedIndex - 1
+
+        Me.Dispose()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Dispose()
+    End Sub
+
+End Class
